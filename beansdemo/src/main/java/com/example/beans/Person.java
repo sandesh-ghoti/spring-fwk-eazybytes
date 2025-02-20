1b5b91c71cde95643d206dd9061c2920eed54437
sandesh ghoti <sandesh.ghoti@gmail.com>
2025-02-21 00:04:51 +0530

    recommonded beans wiring- using autowire annotation

--- beansdemo/src/main/java/com/example/beans/Person.java
+++ beansdemo/src/main/java/com/example/beans/Person.java
@@ -1,7 +1,12 @@
 package com.example.beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Person {
   private String name;
+  @Autowired
   private Vehicle vehicle;
 
   public String getName() {
--- beansdemo/src/main/java/com/example/configuration/config.java
+++ beansdemo/src/main/java/com/example/configuration/config.java
@@ -4,28 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.example.beans.Person;
-import com.example.beans.Vehicle;
-
 @ComponentScan("com.example.beans")
 @Configuration
 public class config {
-  @Bean
-  public Vehicle vehicle() {
-    Vehicle vehicle = new Vehicle();
-    vehicle.setName("audi");
-    vehicle.setMake(2020);
-    return vehicle;
-  }
-
-  @Bean
-  public Person person(Vehicle vehicle) {
-    Person person = new Person();
-    person.setName("sandeep");
-    person.setVehicle(vehicle);
-    return person;
-  }
-
   @Bean
   public String helloWroldString() {
     return "Hello World";
