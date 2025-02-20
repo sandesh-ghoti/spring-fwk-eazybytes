d0265c82a09f8c24f8295c3621bfbed1533bfc87
sandesh ghoti <sandesh.ghoti@gmail.com>
2025-02-20 23:46:08 +0530

    not recommended way- wiring beans

--- beansdemo/src/main/java/com/example/Main.java
+++ beansdemo/src/main/java/com/example/Main.java
@@ -1,14 +1,17 @@
 package com.example;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.example.beans.Person;
 import com.example.beans.Vehicle;
+import com.example.configuration.config;
 
 public class Main {
     public static void main(String[] args) {
-        var context = new ClassPathXmlApplicationContext("beans.xml");
+        var context = new AnnotationConfigApplicationContext(config.class);
         Vehicle vehicle = context.getBean(Vehicle.class);
+        Person person = context.getBean(Person.class);
         System.out.println(vehicle.getName() + " " + vehicle.getMake()); // audi 2020
+        System.out.println(person.getName() + " vehicle: " + person.getVehicle().getName());
         context.close();
     }
 }
---
+++ beansdemo/src/main/java/com/example/beans/Person.java
@@ -0,0 +1,23 @@
+package com.example.beans;
+
+public class Person {
+  private String name;
+  private Vehicle vehicle;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Vehicle getVehicle() {
+    return vehicle;
+  }
+
+  public void setVehicle(Vehicle vehicle) {
+    this.vehicle = vehicle;
+  }
+
+}
