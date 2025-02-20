554519f4dee4e68883c7564ce2daf0b6635b87e8
sandesh ghoti <sandesh.ghoti@gmail.com>
2025-02-20 22:16:20 +0530

    beans creation using xml classpaths

--- beansdemo/src/main/java/com/example/Main.java
+++ beansdemo/src/main/java/com/example/Main.java
@@ -1,25 +1,14 @@
 package com.example;
 
-import java.util.function.Supplier;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-// import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.example.beans.Vehicle;
-import com.example.configuration.config;
 
 public class Main {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(config.class);
-
-        Vehicle vehicle = new Vehicle();
-        Supplier<Vehicle> vehicleSupplier = () -> vehicle;
-        vehicle.setName("audiii-1");
-        context.registerBean("vehicle", Vehicle.class, vehicleSupplier);
-        Vehicle vehicle2 = context.getBean(Vehicle.class);
-
-        vehicle2.setMake(2020);
-        System.out.println(vehicle2.getName() + " " + vehicle2.getMake()); // audiii-1 2020
+        var context = new ClassPathXmlApplicationContext("beans.xml");
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println(vehicle.getName() + " " + vehicle.getMake()); // audi 2020
         context.close();
     }
 }
