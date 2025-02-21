181f25aa5c2d16f7df7c9c6a8b64bf87ccb11b9b
sandesh ghoti <sandesh.ghoti@gmail.com>
2025-02-21 12:04:07 +0530

    achive logging without AOP

--- beansdemo/src/main/java/com/example/services/VehicleService.java
+++ beansdemo/src/main/java/com/example/services/VehicleService.java
@@ -1,5 +1,9 @@
 package com.example.services;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -11,6 +15,7 @@ import com.example.interfaces.Tyre;
 public class VehicleService {
   private Speaker speaker;
   private Tyre tyre;
+  private Logger logger = Logger.getLogger(VehicleService.class.getName());
 
   @Autowired
   public VehicleService(Speaker speaker, @Qualifier("bridgestoneTyre") Tyre tyre) {
@@ -19,10 +24,22 @@ public class VehicleService {
   }
 
   public void music() {
+    Instant start = Instant.now();
+    logger.info("method execution start");
     speaker.playMusic();
+    logger.info("method execution end");
+    Instant finish = Instant.now();
+    long timeElapsed = Duration.between(start, finish).toMillis();
+    logger.info("Time took to execute the method : " + timeElapsed);
   }
 
   public void roll() {
+    Instant start = Instant.now();
+    logger.info("method execution start");
     tyre.rollTyre();
+    logger.info("method execution end");
+    Instant finish = Instant.now();
+    long timeElapsed = Duration.between(start, finish).toMillis();
+    logger.info("Time took to execute the method : " + timeElapsed);
   }
 }
